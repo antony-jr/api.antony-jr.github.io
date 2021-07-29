24f68e5c7575a8d2c43d06c679c8db1dabf64321
antony-jr <antonyjr@protonmail.com>
2021-07-30 00:46:37 +0530

    Allow cors properly

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,8 +1,14 @@
 const TelegramBot = require("node-telegram-bot-api");
 
 const handler = (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://antonyjr.in");
   res.setHeader("Content-Type", "application/json");
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   res.statusCode = 200;
   if (req.method != "POST") {
     res.end(
